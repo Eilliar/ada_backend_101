db5949b4eeef42f1f047facfcaabbc115e41e200
Bruno Eilliar <engbrunoeilliar@gmail.com>
2023-07-17 19:13:43 -0300

    Task 01

---
+++ lec_03/src/task01.java
@@ -0,0 +1,10 @@
+public class task01 {
+    public static double custoCompra(double input) {
+        if(input > 10){
+            return input * 1.25;
+        }
+        else{
+            return input * 1.45;
+        }
+    }
+}
