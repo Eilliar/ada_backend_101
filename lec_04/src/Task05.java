6fa57084d505b1a150e64615b0ccceac76948d1c
Bruno Eilliar <engbrunoeilliar@gmail.com>
2023-07-19 20:51:47 -0300

    Lecture 04 - Task05

---
+++ lec_04/src/Task05.java
@@ -0,0 +1,54 @@
+public class Task05 {
+    public static void main(String[] args) {
+        String[] entradas = {
+                "amor",
+                "cocada",
+                "mora",
+                "ovo",
+                "cachorro",
+                "kaka",
+                "saudades",
+                "palalelepipedo",
+                "fofocalizando",
+                "papagaio",
+                "ateiaateiab"
+        };
+
+        int[] saidas = {
+                0,
+                1,
+                0,
+                1,
+                1,
+                -1,
+                2,
+                9,
+                4,
+                4,
+                10
+        };
+
+        for (int i = 0; i < entradas.length; i++) {
+            double resultado = primeiroCaractereUnico(entradas[i]);
+            double esperado = saidas[i];
+
+            System.out.println("Resultado: " + resultado);
+            System.out.println("Esperado: " + esperado);
+            System.out.println(resultado == esperado);
+            System.out.println();
+        }
+    }
+
+    public static int primeiroCaractereUnico(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char ich = input.charAt(i);
+
+            long count = input.chars().filter(ch -> ch == ich).count();
+            if (count == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
