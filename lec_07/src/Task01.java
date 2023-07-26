3289eef2f93fb1e020706a0c34756f5aceae5790
Bruno Eilliar <engbrunoeilliar@gmail.com>
2023-07-26 20:11:58 -0300

    Lec 07 - Task01

---
+++ lec_07/src/Task01.java
@@ -0,0 +1,41 @@
+enum WeekdayENUM {
+    SEGUNDA(new String[] {"segunda-feira", "segunda", "seg"}),
+    TERCA(new String[] {"terça-feira", "terça", "ter"}),
+    QUARTA(new String[] {"quarta-feira", "quarta", "qua"}),
+    QUINTA(new String[] {"quinta-feira", "quinta", "qui"}),
+    SEXTA(new String[] {"sexta-feira", "sexta", "sex"}),
+    SABADO(new String[] {"sábado", "sab"}),
+    DOMINGO(new String[] {"domingo", "dom"});
+
+    private final String[] keywords;
+    WeekdayENUM(String[] keywords){
+        this.keywords = keywords;
+    }
+
+    public String[] getKeywords(){
+        return keywords;
+    }
+
+    public static WeekdayENUM fromString(String weekDay) {
+        for (WeekdayENUM keywords : WeekdayENUM.values()) {
+            for(String keyword: keywords.getKeywords()){
+                if (keyword.equalsIgnoreCase(weekDay)) {
+                    return keywords;
+                }
+            }
+        }
+        throw new IllegalArgumentException("Invalid input string: " + weekDay);
+    }
+}
+
+public class Task01 {
+
+    public static void main(String[] args) {
+
+        String[] input = {"dom", "segunda-feira", "terça", "qua", "quinta-feira", "sexta", "sab"};
+        for(String day: input){
+            WeekdayENUM dayEnum = WeekdayENUM.fromString(day);
+            System.out.printf("%s -> %s\n", day, dayEnum);
+        }
+    }
+}
